373adaf50fac099bf5e1791a6ae77aa585b82f6a
jonyrh <jonyrh@mail.ru>
2026-06-19 09:44:10 +0500

    Replaced button names with icons

--- jonyrh-multiselect-bs5.js
+++ jonyrh-multiselect-bs5.js
@@ -89,11 +89,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const actionsHtml = showActions ? `
         <div class="d-flex align-items-center row-gap-2">
-          <div class="d-flex gap-2">
-            <button type="button" class="btn btn-link p-0 text-decoration-none btn-sm select-all-btn ${isSelectDisabled}">Выбрать всё</button>
-            <button type="button" class="btn btn-link p-0 text-decoration-none btn-sm text-danger clear-all-btn ${isSelectDisabled}">Сбросить</button>
+          <div class="d-flex gap-1">
+            <button type="button" class="btn btn-link p-0 text-decoration-none btn-sm select-all-btn ${isSelectDisabled}" title="Выбрать всё"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-all" viewBox="0 0 16 16"><path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z"/></svg></button>
+            <button type="button" class="btn btn-link p-0 text-decoration-none btn-sm text-danger clear-all-btn ${isSelectDisabled}" title="Сбросить"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/></svg></button>
           </div>
-          <button type="button" class="btn btn-link p-0 text-decoration-none btn-sm apply-btn ms-auto ${isSelectDisabled}" style="color: var(--bs-success) !important;">Применить</button>
+          <button type="button" class="btn btn-link p-0 text-decoration-none btn-sm apply-btn ms-auto ${isSelectDisabled}" title="Применить" style="color: var(--bs-success) !important;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16"><path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/></svg></button>
         </div>` : '';
 
       headerHtml = `
